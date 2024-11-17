118fd78e620487784059d4466ae764dad0352705
fran <frangomez0409@gmail.com>
2024-11-17 18:34:38 -0300

    feat: footer modificado

--- arfind-react/src/Componentes/Footer/Footer.jsx
+++ arfind-react/src/Componentes/Footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import './Footer.module.css';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p className="footer-text">© Copyright 2024 ARfind</p>
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerLeft}>
+          <p className={styles.footerText}>© 2024 ARfind</p>
+        </div>
+        <div className={styles.footerRight}>
+          <a
+            href="https://sites.google.com/view/arfind-faq/inicio"
+            className={styles.footerLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            FAQ
+          </a>
+          <a
+            href="https://sites.google.com/view/tyc-arfind/inicio"
+            className={styles.footerLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Términos y condiciones
+          </a>
+        </div>
+      </div>
     </footer>
+
   );
 };
 
